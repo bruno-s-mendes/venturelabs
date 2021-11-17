f6aa3554105f2a0ab238d2b5bbd0f37a99a29cf9
Bruno Mendes <mendessbruno@gmail.com>
2021-11-17 13:53:46 -0300

    feat: atualizado sidenav para atualizar estado no app

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -2,18 +2,30 @@ import React from 'react';
 import Sidenav from './components/Sidenav';
 
 class App extends React.Component {
-  // constructor() {
-  //   super();
-  // }
+  constructor() {
+    super();
+    this.state = {
+      page: 'cadastrar',
+    };
+    this.updatePage = this.updatePage.bind(this);
+  }
+
+  updatePage(event) {
+    this.setState({
+      page: event.target.value,
+    })
+  }
 
   render() {
+    const { page } = this.state;
+    console.log(page);
     return (
       <div className="relative min-h-screen flex">
         <div className="bg-blue-800 text-blue-100 w-15 p-4 border-2 border-black">
-        <Sidenav />
+        <Sidenav updtePage={this.updatePage}/>
         </div>
         <div className="flex-1 flex justify-center p-20 border-2 border-black ">
-          conteúdo
+          {page}
         </div>
       </div>
     );
